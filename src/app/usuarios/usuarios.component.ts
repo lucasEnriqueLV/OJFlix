b1c632bf97a0b0e09fd84afbdb4072f21048d1e7
DESKTOP-32G110N\Pichau <lelv@icomp.ufam.edu.br>
2018-10-18 09:54:02 -0400

    Inicio da implementação de injeção do serviço usuários na página Usuários

--- src/app/usuarios/usuarios.component.ts
+++ src/app/usuarios/usuarios.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuariosService } from './usuarios.service'
 
 @Component({
   selector: 'app-usuarios',
   templateUrl: './usuarios.component.html',
-  styleUrls: ['./usuarios.component.css']
+  styleUrls: ['./usuarios.component.css'],
+  providers: [ UsuariosService ]
 })
 export class UsuariosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private usuariosService: UsuariosService) { }
 
   ngOnInit() {
+    this.usuariosService.getUsuarios();
   }
 
 }
---
+++ src/app/usuarios/usuarios.service.ts
@@ -0,0 +1,6 @@
+export class UsuariosService {
+    public getUsuarios(): Array<string> {
+        let usuarios = ['usuario 1', 'usuario 2', 'usuario 3', 'usuario 4'];
+        return usuarios;
+    }
+}
